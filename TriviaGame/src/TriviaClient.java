5d329665e189a98a6e1e818ebc2a8c43bb180c9b
Miguel Najera <migueln1987@gmail.com>
2016-02-24 14:49:02 -0600

    editied TriviaClient.java

--- TriviaGame/src/TriviaClient.java
+++ TriviaGame/src/TriviaClient.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class TriviaClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int port = 6001;
+		InetAddress loopback = InetAddress.getLoopbackAddress();
+		
+		try {
+			Socket clientSocket = new Socket(loopback, port);
+			System.out.println("Connected...");
+		} catch (UnknownHostException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
 	}
-
 }
