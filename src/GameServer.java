6c64e5b5d8f61ba2275bc0b1177c05f30372b51e
Miguel Najera <migueln1987@gmail.com>
2016-02-23 23:12:38 -0600

    Edited GameServer.java

--- src/GameServer.java
+++ src/GameServer.java
@@ -1,2 +1,18 @@
-public class GameServer {
+import java.io.IOException;
+import java.net.Socket;
+
+public class GameServer implements Runnable{
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = 6001;
+        String host = "localhost";
+
+        Socket playerSocket = new Socket(host, portNumber);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
