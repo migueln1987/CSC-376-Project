27de91e9b285e6e5a5a9ca5f85d24ecaa44c4869
lchua09 <lchua09@gmail.com>
2016-02-29 13:16:21 -0600

    Added files via upload
    Added category object

--- TriviaGame/src/Questions.java
+++ TriviaGame/src/Questions.java
@@ -7,18 +7,26 @@ import java.util.Random;
 public class Questions {
 	
 	static String[] csvLine;
+	private String category;
 	private String question;
 	private String option1;
 	private String option2;
 	private String option3;
 	private String option4;
-
-	public Questions(String[] csvLine){
-		this.question = csvLine[1];
-		this.option1 = csvLine[2];
-		this.option2 = csvLine[3];
-		this.option3 = csvLine[4];
-		this.option4 = csvLine[5];
+	
+	
+	public Questions() throws IOException{
+		String [] questionArray = readCSV();
+		this.category = questionArray[0];
+		this.question = questionArray[1];
+		this.option1 = questionArray[2];
+		this.option2 = questionArray[3];
+		this.option3 = questionArray[4];
+		this.option4 = questionArray[5];
+		
+	}
+	public String getCategory(){
+		return category;
 	}
 	
 	public String getQuestion(){
@@ -55,4 +63,7 @@ public class Questions {
 		br.close();
 		return csvLine;
 		}
-	}
+	
+	
+}
+
