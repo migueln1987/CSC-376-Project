0778e72a548b2ae9833f2e883a12642673a71b55
Miguel Najera <migueln1987@gmail.com>
2016-03-02 13:43:16 -0600

    made changes to TriviaGame.java

--- TriviaGame/src/TriviaGame.java
+++ TriviaGame/src/TriviaGame.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class TriviaGame implements Runnable {
@@ -33,76 +32,52 @@ public class TriviaGame implements Runnable {
 	 */
 	public void beginGame() throws IOException
 	{
-		String winner;
-		createQuestion();
+//		ScoreBoard triviaScore = new ScoreBoard();
+		String winner = null;
 		StringBuilder strbld = new StringBuilder();
-//		for (String question : questions.keySet())
-//		{
-		
-		Questions question = new Questions();
+
+		QuestionManager questionManager = new QuestionManager("questions.csv");
+		while (winner == null){
+		Question question = questionManager.getQuestion();
 		output1.println(question.getQuestion());
 		output2.println(question.getQuestion());
-		String right = question.getOption1();
+		String right = question.getRightAnswer();
 			
-//			Collections.shuffle(questions.get(question));
-
-//			for (String option : questions.get(question))
-//			{
 		strbld.append(question.getOption1().toString() + " " + 
-		question.getOption2().toString() + " " + 
-		question.getOption3().toString() + " " +
-		question.getOption4().toString() + " ");
-//			}
-		output1.println(strbld.toString());
-		output2.println(strbld.toString());
+					  question.getOption2().toString() + " " + 
+				      question.getOption3().toString() + " " +
+				      question.getOption4().toString() + " ");
+			output1.println(strbld.toString());
+			output2.println(strbld.toString());
 			
-		String[] response1 = input1.readLine().split(",");
-		String[] response2 = input2.readLine().split(",");
-		
-		if (Integer.valueOf(response1[1]) < Integer.valueOf(response2[1]) )
-		{
-			if (response1[0].equals(right)) winner = "player1";
+			String[] response1 = input1.readLine().split(",");
+			String[] response2 = input2.readLine().split(",");
+			
+			if (Integer.valueOf(response1[1]) < Integer.valueOf(response2[1]) )
+			{
+				if (response1[0].equals(right)) winner = "player1";
 				
-			else if (response2[0].equals(right)) winner = "player2";
+				else if (response2[0].equals(right)) winner = "player2";
 				
-			else winner = "both wrong";
+				else winner = "both wrong";
+			}
+			else
+			{
+				if (response2[0].equals(right)) winner = "player1";
+				
+				else if (response1[0].equals(right)) winner = "player2";
+				
+				else winner = "both wrong";
+				
+			}
+			System.out.println("player one says: " + response1.toString() + "\n"
+					+ "player two says: " + response2.toString());
 		}
-		else
-		{
-			if (response2[0].equals(right)) winner = "player1";
-				
-			else if (response1[0].equals(right)) winner = "player2";
-				
-			else winner = "both wrong";
-				
-		}
-		String player1_an = response1[0];
-		String player2_an = response2[0];
 			
-		System.out.println("player one says: " + player2_an + "\n"
-		+ "player two says: " + player1_an);
-			
-		System.out.println("winner: " + winner);
-//		}
+			System.out.println("winner: " + winner);		
 		
 	}
 	
-	/**
-	 * placeholder for the questions class
-	 */
-	public void createQuestion()
-	{
-		questions = new HashMap<String, ArrayList<String>>();
-		String q = "What company created Java?";
-		ArrayList<String> options = new ArrayList<String>();
-		options.add("Sun Microsystems");
-		options.add("Oracle");
-		options.add("Microsoft");
-		options.add("Google");
-		
-		questions.put(q, options);
-		
-	}
 	@Override
 	public void run() {
 		try {
