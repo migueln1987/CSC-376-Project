70c10f30b024c3a43353cbe6b0bff6c3caa71358
Tom <twozni94@gmail.com>
2016-03-03 02:11:26 -0600

    Simplified game by allowing the typing of only characters.
    Fixed printing out of questions.

--- TriviaGame/src/TriviaClient.java
+++ TriviaGame/src/TriviaClient.java
@@ -9,96 +9,98 @@ import java.util.Date;
 
 
 public class TriviaClient {
-	
-	BufferedReader in;
-	PrintWriter output;
-	BufferedReader stdIn;
-	Socket clientSocket;
-	InetAddress loopback = InetAddress.getLoopbackAddress();
-	public TriviaClient() throws IOException
-	{
-		clientSocket = new Socket(loopback, 6001);
-		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		stdIn = new BufferedReader(new InputStreamReader(System.in));
-		output= new PrintWriter( clientSocket.getOutputStream(), true );
-		
-	}
-	
-	public void startGame()
-	{
-		try {
-			
-			System.out.println("Connected...");
-			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			stdIn = new BufferedReader(new InputStreamReader(System.in));
-			PrintWriter output= new PrintWriter( clientSocket.getOutputStream(), true );
-			//start game. will be in a loop for multiple questions
-			String answer = "nothing";
-			String correct_answer, message;
-			for (int i = 0; i < 10; i++) {
-				// question
-				System.out.println(in.readLine());
 
-				//options
-				System.out.println(in.readLine());
-		
-		
-				System.out.println("Enter Response: ");
+    BufferedReader in;
+    PrintWriter output;
+    BufferedReader stdIn;
+    Socket clientSocket;
+    InetAddress loopback = InetAddress.getLoopbackAddress();
 
-			
-				long d1 = new Date().getTime();
-		
-				long startTime = System.currentTimeMillis();
-				while ((System.currentTimeMillis() - startTime) < 20 * 1000
-					&& !stdIn.ready()) {
-				}
+    public TriviaClient() throws IOException {
+        clientSocket = new Socket(loopback, 6001);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+        output = new PrintWriter(clientSocket.getOutputStream(), true);
 
-				if (stdIn.ready()) {
-					answer = stdIn.readLine();
-					//System.out.println("You entered: " + answer);
-				
-				} 	
-				else {
-					answer = "wrong";
-					//System.out.println("You did not enter data");
-		    
-				}
-		
+    }
 
-				long d2 = new Date().getTime();
-				long answer_time = d2 - d1;
-				output.println(answer + "," + Integer.toString((int) answer_time));
-				
-				correct_answer = in.readLine();
-				//message = in.readLine();
-				System.out.println(correct_answer);
-				//System.out.println(message);
-			
-				
-			}
-			
-			System.out.println(in.readLine());
-			clientSocket.close();
-		
-	} catch (UnknownHostException e) {
-		System.err.println(e);
-		e.printStackTrace();
-	} catch (IOException e) {
-		System.err.println(e);
-		e.printStackTrace();
-	}
-	}
+    public void startGame() {
+        try {
 
-	public static void main(String[] args) {
-		try
-		{
-		TriviaClient tc = new TriviaClient();
-		tc.startGame();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            System.out.println("Connected...");
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            //start game. will be in a loop for multiple questions
+            String answer = "nothing";
+            String correct_answer, message;
+            for (int i = 0; i < 10; i++) {
+                // question
+                System.out.println(in.readLine());
+                System.out.println();
+
+                //options
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
+
+
+                System.out.println();
+                System.out.println("Enter Response: ");
+
+
+                long d1 = new Date().getTime();
+
+                long startTime = System.currentTimeMillis();
+                while ((System.currentTimeMillis() - startTime) < 20 * 1000
+                        && !stdIn.ready()) {
+                }
+
+                if (stdIn.ready()) {
+                    answer = stdIn.readLine();
+                    //System.out.println("You entered: " + answer);
+
+                } else {
+                    answer = "wrong";
+                    //System.out.println("You did not enter data");
+
+                }
+
+
+                long d2 = new Date().getTime();
+                long answer_time = d2 - d1;
+                output.println(answer + "," + Integer.toString((int) answer_time));
+
+                correct_answer = in.readLine();
+                //message = in.readLine();
+                System.out.println(correct_answer);
+                //System.out.println(message);
+
+                System.out.println(in.readLine());
+
+
+            }
+
+            System.out.println(in.readLine());
+            clientSocket.close();
+
+        } catch (UnknownHostException e) {
+            System.err.println(e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            TriviaClient tc = new TriviaClient();
+            tc.startGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
--- TriviaGame/src/TriviaGame.java
+++ TriviaGame/src/TriviaGame.java
@@ -1,99 +1,114 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class TriviaGame implements Runnable {
-	Socket player1, player2;
-	int score1, score2;
-	private BufferedReader input1, input2;
-	private PrintWriter output1, output2;
-	
-	public HashMap<String, ArrayList<String>> questions;
-	public TriviaGame(ArrayList<Socket> clients) throws IOException
-	{
-		player1 = clients.get(0);
-		input1 = new BufferedReader( 
-				 new InputStreamReader(player1.getInputStream()) );
-		output1= new PrintWriter( player1.getOutputStream(), true );
-		
-		
-		player2 = clients.get(1);
-		input2 = new BufferedReader( 
-				 new InputStreamReader(player2.getInputStream()) );
-		output2 = new PrintWriter( player2.getOutputStream(), true );
-	}
-	
-	/**
-	 * for testing... Actually game will have more complex questions
-	 * @throws IOException 
-	 */
-	public void beginGame() throws IOException
-	{
-//		ScoreBoard triviaScore = new ScoreBoard();
-		String winner = null;
-		StringBuilder strbld = new StringBuilder();
-		int count = 0;
-		QuestionManager questionManager = new QuestionManager("questions.csv");
-		
-		while (count < 10){
-	
-		Question question = questionManager.getQuestion();
-		output1.println(question.getQuestion());
-		output2.println(question.getQuestion());
-		String right = question.getRightAnswer();
-			
-		strbld.append(question.getOption1().toString() + " " + 
-					  question.getOption2().toString() + " " + 
-				      question.getOption3().toString() + " " +
-				      question.getOption4().toString() + " ");
-			output1.println(strbld.toString());
-			output2.println(strbld.toString());
-			
-			String[] response1 = input1.readLine().split(",");
-			String[] response2 = input2.readLine().split(",");
-			
+    Socket player1, player2;
+    int score1, score2;
+    private BufferedReader input1, input2;
+    private PrintWriter output1, output2;
 
-			
-			if (Integer.valueOf(response1[1]) < Integer.valueOf(response2[1]) )
-			{
-				if (response1[0].equals(right)) {
-					winner = "player1";
-					score1++;
-				}
-				
-				else if (response2[0].equals(right)) {
-					winner = "player2";
-					score2++;
-				}
-				
-				else winner = "both wrong";
-			}
-			else
-			{
-				if (response2[0].equals(right)) {
-					winner = "player2";
-					score2++;
-				}
-				
-				else if (response1[0].equals(right)) {
-					winner = "player1";
-					score1++;
-				}
-				
-				else winner = "both wrong";
-				
-			}
-			System.out.println("player one says: " + response1[0] + "\n"
-					+ "player two says: " + response2[0]);
-			
-			
-			output1.println("Correct Answer: " + right + ". Your Score: " + score1);
-			output2.println("Correct Answer: " + right + ". Your Score: " + score2);
-			/*
+    public HashMap<String, ArrayList<String>> questions;
+
+    public TriviaGame(ArrayList<Socket> clients) throws IOException {
+        player1 = clients.get(0);
+        input1 = new BufferedReader(
+                new InputStreamReader(player1.getInputStream()));
+        output1 = new PrintWriter(player1.getOutputStream(), true);
+
+
+
+
+        player2 = clients.get(1);
+        input2 = new BufferedReader(
+                new InputStreamReader(player2.getInputStream()));
+        output2 = new PrintWriter(player2.getOutputStream(), true);
+    }
+
+    /**
+     * for testing... Actually game will have more complex questions
+     *
+     * @throws IOException
+     */
+    public void beginGame() throws IOException {
+//		ScoreBoard triviaScore = new ScoreBoard();
+        String winner = null;
+        StringBuilder strbld = new StringBuilder();
+        int count = 0;
+        QuestionManager questionManager = new QuestionManager("questions.csv");
+
+        while (count < 10) {
+
+            List<String> shuffle = new ArrayList<>();
+
+            Question question = questionManager.getQuestion();
+            output1.println(question.getQuestion());
+            output2.println(question.getQuestion());
+            String right = question.getRightAnswer();
+
+            shuffle.add(question.getOption1());
+            shuffle.add(question.getOption2());
+            shuffle.add(question.getOption3());
+            shuffle.add(question.getOption4());
+
+            Collections.shuffle(shuffle);
+
+            List<String> answerList = new ArrayList<>();
+
+            answerList.add("A) " + shuffle.get(0));
+            answerList.add("B) " + shuffle.get(1));
+            answerList.add("C) " + shuffle.get(2));
+            answerList.add("D) " + shuffle.get(3));
+
+            String newLine = System.getProperty("line.separator");
+
+            strbld.append(answerList.get(0) + newLine +
+            answerList.get(1) + newLine +
+                    answerList.get(2) + newLine +
+                    answerList.get(3));
+
+            output1.println(strbld.toString());
+            output2.println(strbld.toString());
+
+            String[] response1 = input1.readLine().split(",");
+            String[] response2 = input2.readLine().split(",");
+
+
+            String player1Response = response1[0];
+            String player2Response = response2[0];
+
+            for (String s : answerList){
+                System.out.println("Right answer is " + right);
+                if (s.substring(3, s.length()).trim().toLowerCase().equals(right.toLowerCase())){
+                    right = s;
+                }
+            }
+
+            if (Integer.valueOf(response1[1]) < Integer.valueOf(response2[1])) {
+                if (player1Response.toLowerCase().equals(right.substring(0,1).toLowerCase())) {
+                    winner = "player1";
+                    score1++;
+                } else if (player2Response.toLowerCase().equals(right.substring(0,1).toLowerCase())) {
+                    winner = "player2";
+                    score2++;
+                } else winner = "both wrong";
+            } else {
+                if (player2Response.toLowerCase().equals(right.substring(0,1).toLowerCase())) {
+                    winner = "player2";
+                    score2++;
+                } else if (player1Response.toLowerCase().equals(right.substring(0,1).toLowerCase())) {
+                    winner = "player1";
+                    score1++;
+                } else winner = "both wrong";
+
+            }
+            System.out.println("player one says: " + response1[0] + "\n"
+                    + "player two says: " + response2[0]);
+
+
+            output1.println("Correct Answer: " + right + ". Your Score: " + score1 + newLine);
+            output2.println("Correct Answer: " + right + ". Your Score: " + score2 + newLine);
+            /*
 			if (winner.equals("player1"))
 			{
 				output1.println("you (player1) ansewered: " + response1[0] + "in " + response1[1] + " seconds"+ "\n" 
@@ -125,55 +140,49 @@ public class TriviaGame implements Runnable {
 						+ "YOU LOSE THE ROUND! YOUR CURRENT SCORE IS " + score2);
 			}
 			*/
-			//output1.flush();
-			//output2.flush();
-			strbld = new StringBuilder();
-			count++;
-			
-		}
-			if (score1 > score2)
-			{
-				output1.println("you win, congrats you're smarter than your opponent.\n"
-						+ " Your Final Score: " + score1 + "/n"
-						+  "Opponents Final Score: " + score2); 
-						
-				output2.println("You Lose, Better Luck Next Time.\n"
-						+ " Your Final Score: " + score2 + "/n"
-						+  "Opponents Final Score: " + score1);
-			}
-			
-			if (score2 > score1)
-			{
-				output2.println("you win, congrats you're smarter than your opponent.\n"
-						+ " Your Final Score: " + score1 + "/n"
-						+  "Opponents Final Score: " + score2); 
-						
-				output1.println("You Lose, Better Luck Next Time.\n"
-						+ " Your Final Score: " + score2 + "/n"
-						+  "Opponents Final Score: " + score1);
-			}
-			else
-			{
-				output2.println("Tie\n"
-						+ " Your Final Score: " + score2 + "/n"
-						+  "Opponents Final Score: " + score1);
-				output1.println("Tie\n"
-						+ " Your Final Score: " + score2 + "/n"
-						+  "Opponents Final Score: " + score1);
-			}
-			System.out.println("winner: " + winner);		
-		
-	}
-	
-	@Override
-	public void run() {
-		try {
-			beginGame();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            //output1.flush();
+            //output2.flush();
+            strbld = new StringBuilder();
+            count++;
+
+        }
+        if (score1 > score2) {
+            output1.println("you win, congrats you're smarter than your opponent.\n"
+                    + " Your Final Score: " + score1 + "/n"
+                    + "Opponents Final Score: " + score2);
+
+            output2.println("You Lose, Better Luck Next Time.\n"
+                    + " Your Final Score: " + score2 + "/n"
+                    + "Opponents Final Score: " + score1);
+        }
+
+        if (score2 > score1) {
+            output2.println("you win, congrats you're smarter than your opponent.\n"
+                    + " Your Final Score: " + score1 + "/n"
+                    + "Opponents Final Score: " + score2);
+
+            output1.println("You Lose, Better Luck Next Time.\n"
+                    + " Your Final Score: " + score2 + "/n"
+                    + "Opponents Final Score: " + score1);
+        } else {
+            output2.println("Tie\n"
+                    + " Your Final Score: " + score2 + "/n"
+                    + "Opponents Final Score: " + score1);
+            output1.println("Tie\n"
+                    + " Your Final Score: " + score2 + "/n"
+                    + "Opponents Final Score: " + score1);
+        }
+        System.out.println("winner: " + winner);
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            beginGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
